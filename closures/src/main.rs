7d0dd070e93783d92a10c9f227dc53c98005edd8
Crash <1903291+hex22a@users.noreply.github.com>
2026-08-11 15:49:46 +0400

    move

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let list = vec![1, 2, 3];
     println!("Before defining closure: {list:?}");
 
-    thread::spawn(|| println!("From thread: {list:?}"))
+    thread::spawn(move || println!("From thread: {list:?}"))
         .join()
         .unwrap();
 }
