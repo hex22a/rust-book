5d0c21973da8c3c62859a63613e7fe35f9c2fb68
Crash <1903291+hex22a@users.noreply.github.com>
2026-08-12 16:27:15 +0400

    async

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,29 +1,18 @@
-use trpl::{Either, Html};
+use std::time::Duration;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
     trpl::block_on(async {
-        let title_fut_1 = page_title(&args[1]);
-        let title_fut_2 = page_title(&args[2]);
-
-        let (url, maybe_title) = match trpl::select(title_fut_1, title_fut_2).await {
-            Either::Left(left) => left,
-            Either::Right(right) => right,
+        let fut1 = async {
+            for i in 1..10 {
+                println!("hi number {i} from the first task!");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
         };
 
-        println!("{url} returned first");
-        match maybe_title {
-            Some(title) => println!("Its page title was: '{title}'"),
-            None => println!("It had no title."),
+        for i in 1..5 {
+            println!("hi number {i} from the second task!");
+            trpl::sleep(Duration::from_millis(500)).await;
         }
-    })
-}
-
-async fn page_title(url: &str) -> (&str, Option<String>) {
-    let response_text = trpl::get(url).await.text().await;
-    let title = Html::parse(&response_text)
-        .select_first("title")
-        .map(|title| title.inner_html());
-    (url, title)
+        fut1.await;
+    });
 }
