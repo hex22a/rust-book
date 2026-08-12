0d9cff3b5103a8e0e9bf92d94a4c52fefad6bb6c
Crash <1903291+hex22a@users.noreply.github.com>
2026-08-12 16:38:59 +0400

    async

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -2,17 +2,27 @@ use std::time::Duration;
 
 fn main() {
     trpl::block_on(async {
-        let fut1 = async {
-            for i in 1..10 {
-                println!("hi number {i} from the first task!");
+        let (tx, mut rx) = trpl::channel();
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(500)).await;
             }
         };
 
-        for i in 1..5 {
-            println!("hi number {i} from the second task!");
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
-        fut1.await;
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
+            }
+        };
+
+        trpl::join(tx_fut, rx_fut).await;
     });
 }
